e03a5a936d4c07435861f2aa6ebc1b1ec8e102f6
cr3pt <cr3pt@o2.pl>
2014-09-08 10:25:41 +0200

    changes changes...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,11 +5,11 @@ include device/lge/msm7x27a-common/BoardConfigCommon.mk
 LGE_PROJECT := l
 
 # inherit from the proprietary version
--include vendor/lge/p705/BoardConfigVendor.mk
+-include vendor/lge/p700/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=u0 lge.signed_image=false
+BOARD_KERNEL_CMDLINE := androidboot.hardware=u0 androidboot.selinux=permissive lge.signed_image=false
 
 TARGET_PREBUILT_KERNEL := device/lge/p705/kernel
 
@@ -20,7 +20,7 @@ TARGET_KERNEL_SOURCE := kernel/lge/msm7x27a-common
 TARGET_BOOTLOADER_BOARD_NAME := p705
 TARGET_KERNEL_CONFIG := cyanogenmod_u0_nonfc_defconfig
 
-TARGET_RECOVERY_FSTAB := device/lge/p705/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/lge/p700/recovery.fstab
 
 ## We need this for the flipped screen
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p705/recovery/graphics.c
